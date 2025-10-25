d929fb19add2fe7096520f1a1f93d6421f2086f9
sreenugaddam <sreenu.gaddam@gmail.com>
2025-10-25 18:46:53 +0530

    commit

---
+++ basics/practice1.py
@@ -0,0 +1,36 @@
+# python practice 1
+a = 10
+b = 20
+c = a+b
+d = 'python'
+print(c,d)
+
+Student_name = 'Vehanth Sai'
+print(type(Student_name)) 
+Student_age = 1
+print(type(Student_age)) 
+Student_gpa = 9.9
+print(type(Student_gpa)) 
+
+
+Student_name = 'Vehanth Sai'
+print(Student_name)
+Student_age = 1
+print(Student_age)
+Student_gpa = 10
+print(Student_gpa)
+
+x = 40
+y = 50
+z = 'Sai'
+print(x+y,z)
+
+x=y=z= 'one'
+print(x)
+print(y)
+print(z)
+
+x = 'Vehanth'
+y = ' sai'
+z = ' Gaddam'
+print(x+y+z)
